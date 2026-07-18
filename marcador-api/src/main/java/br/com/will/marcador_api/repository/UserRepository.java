6bbd5f4f9a7174e244ee6330196dfec28fdd96f5
willUlisses <williamcavalcante06@gmail.com>
2026-07-18 17:34:23 -0300

    feat: novo método pra carregar usuário pelo username pra ser usado no UserDetailsService

--- marcador-api/src/main/java/br/com/will/marcador_api/repository/UserRepository.java
+++ marcador-api/src/main/java/br/com/will/marcador_api/repository/UserRepository.java
@@ -3,6 +3,9 @@ package br.com.will.marcador_api.repository;
 import br.com.will.marcador_api.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    Optional<User> findByUsername(String username);
 }
