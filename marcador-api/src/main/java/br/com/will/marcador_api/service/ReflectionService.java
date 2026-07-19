1d646a9248c2d5cc4b0ff6d32f2399544908048c
willUlisses <williamcavalcante06@gmail.com>
2026-07-19 00:42:32 -0300

    feat: criação do CREATE de reflexões

--- marcador-api/src/main/java/br/com/will/marcador_api/service/ReflectionService.java
+++ marcador-api/src/main/java/br/com/will/marcador_api/service/ReflectionService.java
@@ -1,4 +1,38 @@
 package br.com.will.marcador_api.service;
 
+import br.com.will.marcador_api.dtos.body.CreateReflectionBody;
+import br.com.will.marcador_api.dtos.response.BookWithReflectionsResponse;
+import br.com.will.marcador_api.entities.Book;
+import br.com.will.marcador_api.entities.Reflection;
+import br.com.will.marcador_api.entities.User;
+import br.com.will.marcador_api.exception.NotFoundException;
+import br.com.will.marcador_api.exception.UnauthorizedException;
+import br.com.will.marcador_api.repository.BookRepository;
+import br.com.will.marcador_api.repository.ReflectionsRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class ReflectionService {
+
+    private final ReflectionsRepository reflectionsRepository;
+    private final BookRepository bookRepository;
+
+    public BookWithReflectionsResponse createReflection(User user, Long bookId, CreateReflectionBody body) {
+        Book book = bookRepository.findById(bookId)
+                .orElseThrow(() -> new NotFoundException("Book not found"));
+
+        if (!book.getUser().getId().equals(user.getId())) {
+            throw new UnauthorizedException("This book does not belong to this user");
+        }
+
+        Reflection reflection = new Reflection();
+        reflection.setDescription(body.description());
+        reflection.setTitle(body.title());
+        book.addReflection(reflection);
+
+        return BookWithReflectionsResponse.from(bookRepository.save(book));
+    }
+
 }
