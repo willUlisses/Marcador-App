6fa5899f5d809c0292cc63dbbfe8da091df96251
willUlisses <williamcavalcante06@gmail.com>
2026-07-20 14:54:17 -0300

    feat: método no repository pra puxar as reflections direto pelo book id

--- marcador-api/src/main/java/br/com/will/marcador_api/repository/ReflectionsRepository.java
+++ marcador-api/src/main/java/br/com/will/marcador_api/repository/ReflectionsRepository.java
@@ -1,7 +1,13 @@
 package br.com.will.marcador_api.repository;
 
+import br.com.will.marcador_api.entities.Book;
 import br.com.will.marcador_api.entities.Reflection;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReflectionsRepository extends JpaRepository<Reflection, Long> {
+
+    List<Reflection> findAllByBookId(Long bookId);
+
 }
