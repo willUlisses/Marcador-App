5af74eb396e9d98ce5800ae6c84c6e5075f0dfe0
willUlisses <williamcavalcante06@gmail.com>
2026-07-21 11:32:08 -0300

    feat: repository da entidade de PasswordResetToken

--- marcador-api/src/main/java/br/com/will/marcador_api/repository/PasswordResetTokenRepository.java
+++ marcador-api/src/main/java/br/com/will/marcador_api/repository/PasswordResetTokenRepository.java
@@ -1,4 +1,17 @@
 package br.com.will.marcador_api.repository;
 
-public class PasswordResetTokenRepository {
+import br.com.will.marcador_api.entities.PasswordResetToken;
+import br.com.will.marcador_api.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
+
+    Optional<PasswordResetToken> findByToken(String token);
+
+    void deleteByUser(User user);
+
 }
