3e7eba26b2cb84f904960a5ad4a9b1513a22cdc4
willUlisses <williamcavalcante06@gmail.com>
2026-08-11 16:20:05 -0300

    feat: adicionando configuração de rotas das páginas utilizando react-router-dom

--- marcador-front/marcador/src/App.tsx
+++ marcador-front/marcador/src/App.tsx
@@ -1,10 +1,23 @@
 import  LoginPage  from './Pages/LoginPage'
+import RegisterPage from './Pages/RegisterPage.tsx'
+import ProfilePage from './Pages/ProfilePage'
+import LibraryPage from './Pages/LibraryPage'
+import ShelfPage from './Pages/ShelfPage'
+import StatsPage from './Pages/StatsPage'
+import ReflectionsPage from './Pages/ReflectionsPage'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className='flex flex-col w-screen h-dvh bg-amber-100'>
-      <LoginPage />
-    </div>
+    <Routes>
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/library" element={<LibraryPage />} />
+      <Route path="/shelf" element={<ShelfPage />} />
+      <Route path="/stats" element={<StatsPage />} />
+      <Route path="/reflections" element={<ReflectionsPage />} />
+      <Route path="/profile" element={<ProfilePage />} />
+    </Routes>
   )
 }
 
