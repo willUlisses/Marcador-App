04ed1a45ca09a8dc04cd078497bdbafe64d45ab9
willUlisses <williamcavalcante06@gmail.com>
2026-08-16 00:01:13 -0300

    feat: adicionando seção de ReadingNow para mostrar num carrossel os livros que estão sendo lidos atualmente com informações sobre o estado da leitura

--- marcador-front/marcador/src/Pages/ShelfPage.tsx
+++ marcador-front/marcador/src/Pages/ShelfPage.tsx
@@ -1,11 +1,22 @@
 import MobileNav from "../components/MobileNav"
 import { useAuth } from "../contexts/AuthContext"
+import ReadingNow from "../components/ReadingNow"
+import { useEffect, useState } from "react"
+import type { BookResponse } from "../schemas/book"
+import { bookService } from "../services/bookService"
 
 const ShelfPage = () => {
     const { user } = useAuth()
 
+    const [books, setBooks] = useState<BookResponse[]>([])
+
+    useEffect(() => {
+        bookService.getAllCompletedBooks()
+        .then(response => setBooks(response));
+    }, [])
+
     return (
-        <div className="flex flex-col w-full min-h-screen p-4 gap-4 bg-[#fcf6ec]">
+        <div className="flex flex-col w-full min-h-screen p-4 gap-4 bg-[#fcf9f5]">
             <header>
                 <p className="text-black text-md font-source">Olá,</p>
                 <h1 className="text-black text-3xl font-lora font-bold tracking-wider">{user.username}</h1>
@@ -13,9 +24,7 @@ const ShelfPage = () => {
 
             <main>
                 <h1 className="font-lora text-xl tracking-wide">Lendo Agora</h1>
-                <div className="">
-                    fazer componente pra mostrar o livro que está sendo lido atualmente
-                </div>
+                <ReadingNow books={books}  />
                 <br />
                 <div>div com gráfico da quantidade de páginas lidas na semana (em cada dia e total)</div>
             </main>
