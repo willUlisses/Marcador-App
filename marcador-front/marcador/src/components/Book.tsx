1215be4b96847c0b212ea18158105a828606ba0e
willUlisses <williamcavalcante06@gmail.com>
2026-08-15 23:37:19 -0300

    feat: adicionando cores randomicas com base no id pras capas dos componentes dos livros

--- marcador-front/marcador/src/components/Book.tsx
+++ marcador-front/marcador/src/components/Book.tsx
@@ -11,12 +11,20 @@ interface BookProps {
     opinion: string;
 }
 
+const bookColours: string[] = ["#7A3B2E", "#5C1F2E", "#4B5A3F", "#3D3020", "#4A3D6B", "#8A7562", "#6B5D4F", "#2E4A3A"];
+const bookBorderColours: string[] = ["#613029", "#401521", "#394733", "#2F241A", "#373155", "#6B5D4F", "#574E44", "#2E4A3A"]
+
 const Book = ({ id, title, genres, status, currentPage, totalPages, rating, opinion }: BookProps) => {
     return (
         <div
-            className="flex flex-col justify-evenly items-center w-28 h-46 bg-amber-900 px-2 border-l-2 border-amber-950 rounded-md">
+            style={{ 
+                backgroundColor: bookColours[id % bookColours.length], 
+                borderLeftColor: bookBorderColours[id % bookBorderColours.length] 
+            }}
+            className="flex flex-col justify-evenly items-center w-28 h-46 px-2 border-l-[5px] rounded-lg">
+                
             <span
-                className={`text-[12px] font-lora text-center ${status == "COMPLETADO" ? "rounded-full bg-green-700 p-1" : "bg-amber-400 rounded-md px-2 text-center"}`}>
+                className={`text-[11px] font-bold tracking-widest text-center ${status == "COMPLETADO" ? "rounded-full bg-green-700 p-1" : "bg-amber-400 rounded-md px-2 text-center"}`}>
                 {status === "COMPLETADO" ? <Check className="text-white" size={14} strokeWidth={4} /> : status.replace("_", " ")}
             </span>
 
