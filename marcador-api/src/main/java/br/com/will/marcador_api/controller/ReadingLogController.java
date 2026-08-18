5ede5bb1a2a5c0288f596e58de5b2a2c7707869d
willUlisses <williamcavalcante06@gmail.com>
2026-08-18 00:55:45 -0300

    feat: criando a rota para retornar as páginas lidas em cada dia da semana atual

--- marcador-api/src/main/java/br/com/will/marcador_api/controller/ReadingLogController.java
+++ marcador-api/src/main/java/br/com/will/marcador_api/controller/ReadingLogController.java
@@ -1,4 +1,26 @@
 package br.com.will.marcador_api.controller;
 
+import br.com.will.marcador_api.dtos.response.WeeklyProgressResponse;
+import br.com.will.marcador_api.entities.User;
+import br.com.will.marcador_api.service.ReadingLogService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/marcador/reading-logs")
+@RequiredArgsConstructor
 public class ReadingLogController {
+
+    private final ReadingLogService readingLogService;
+
+
+    @GetMapping("/weekly")
+    public ResponseEntity<WeeklyProgressResponse> getWeeklyProgressData(@AuthenticationPrincipal User user) {
+        return new ResponseEntity<>(readingLogService.getWeeklyProgress(user), HttpStatus.OK);
+    }
 }
