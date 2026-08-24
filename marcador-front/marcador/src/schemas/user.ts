089e68bc2b2d3743f81d107e0a98abfc65f34b27
willUlisses <williamcavalcante06@gmail.com>
2026-08-24 13:14:20 -0300

    feat: criando schema de response do endpoint que retorna os status de leitura geral do usuário

--- marcador-front/marcador/src/schemas/user.ts
+++ marcador-front/marcador/src/schemas/user.ts
@@ -14,3 +14,9 @@ export interface ChangePasswordBody {
     currentPassword: string,
     newPassword: string,
 }
+
+export interface UserStatsResponse {
+    books_read: number;
+    books_in_queue: number;
+    total_pages_read: number;
+}
