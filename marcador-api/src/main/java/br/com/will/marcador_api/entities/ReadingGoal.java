b6aa12fa07858ec1d1703eb52289ba14713a38b6
willUlisses <williamcavalcante06@gmail.com>
2026-09-23 23:51:32 -0300

    feat: adicionando entidade que representa metas de leitura anual

---
+++ marcador-api/src/main/java/br/com/will/marcador_api/entities/ReadingGoal.java
@@ -0,0 +1,44 @@
+package br.com.will.marcador_api.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "tb_goals")
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ReadingGoal {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "target_year", nullable = false)
+    private Integer year;
+
+    @Column(name = "target_books", nullable = false)
+    private Integer targetBooks;
+
+
+
+
+}
