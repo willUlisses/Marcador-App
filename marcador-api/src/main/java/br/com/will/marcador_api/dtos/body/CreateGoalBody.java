6d502785e8faed34b7926562178692521ebb678a
willUlisses <williamcavalcante06@gmail.com>
2026-09-23 23:52:05 -0300

    feat: DTOs de body e response das metas anuais

--- marcador-api/src/main/java/br/com/will/marcador_api/dtos/body/CreateGoalBody.java
+++ marcador-api/src/main/java/br/com/will/marcador_api/dtos/body/CreateGoalBody.java
@@ -1,4 +1,11 @@
 package br.com.will.marcador_api.dtos.body;
 
-public class CreateGoalBody {
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+public record CreateGoalBody(
+        @NotNull(message = "Você deve definir uma quantidade de livros pra meta")
+        @Min(value = 1, message = "Você deve ter no mínimo 1 livro como meta anual")
+        Integer targetBooks
+) {
 }
--- marcador-api/src/main/java/br/com/will/marcador_api/dtos/response/ReadingGoalResponse.java
+++ marcador-api/src/main/java/br/com/will/marcador_api/dtos/response/ReadingGoalResponse.java
@@ -1,4 +1,23 @@
 package br.com.will.marcador_api.dtos.response;
 
-public class ReadingGoalResponse {
+public record ReadingGoalResponse(
+        Integer year,
+        Integer targetBooks,
+        Long completedBooks,
+        Double progressPercentage,
+        Boolean isCompleted
+) {
+    public static ReadingGoalResponse of(Integer targetBooks, Integer year, Long completedBooks) {
+        double percentage = targetBooks > 0 ? (completedBooks.doubleValue() / targetBooks) * 100 : 0.0;
+        double formattedPercentage = Math.min(Math.round(percentage * 10.0) / 10.0, 100.0);
+
+        return new ReadingGoalResponse(
+                year,
+                targetBooks,
+                completedBooks,
+                formattedPercentage,
+                completedBooks >= targetBooks
+        );
+    }
+    
 }
