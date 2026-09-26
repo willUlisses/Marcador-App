b090117ff2203150b48fc6d90dcb9a89aed70012
willUlisses <williamcavalcante06@gmail.com>
2026-09-25 23:54:34 -0300

    hotfix: corrigindo a identação do service

--- marcador-front/marcador/src/services/readingGoalService.ts
+++ marcador-front/marcador/src/services/readingGoalService.ts
@@ -4,8 +4,6 @@ import { api } from "./api";
 const currentYear = new Date().getFullYear();
 
 export const readingGoalService = {
-    
-
     getGoalProgress: () => api.get<ReadingGoalResponse>("/goals"),
     upsertGoal: (body: CreateGoalBody) => api.put<ReadingGoalResponse>(`/goals/${currentYear}`, body),
     deleteGoal: () => api.delete(`/goals/${currentYear}`)
